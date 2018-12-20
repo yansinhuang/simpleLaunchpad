e9fa807403a3545d619d471ccb40e789db744b5a
YansinHuang <ypps930101@gmail.com>
2018-12-20 19:30:04 +0800

    rrr
    rrr

--- hw 3-2.py
+++
@@ -1,7 +0,0 @@
-import datetime
-lst_date1 = [int(i) for i in input().split()]
-d = int(input())
-
-date1 = datetime.datetime(lst_date1[0], lst_date1[1], lst_date1[2])
-
-print((date1 + datetime.timedelta(days = d)).strftime('%Y-%m-%d'))
